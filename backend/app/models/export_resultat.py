eb81f4eeaa3ed8b477f78f4e518038bcf10c70da
ISSAD-Dembele <issaddembele25@gmail.com>
2026-06-27 02:48:05 +0100

    update du fichier export_resultat

--- backend/app/models/export_resultat.py
+++ backend/app/models/export_resultat.py
@@ -5,5 +5,5 @@ export_resultat=Table(
     "export_resultat",
     Base.metadata,
     Column("export_id", Integer, ForeignKey("exports_pdf.export_id"), primary_key=True),
-    Column("resultat_id", Integer, ForeignKey("resultats_chatbot.resultat_id"), primary_key=True)
+    Column("resultat_id", Integer, ForeignKey("resultats_chatbots.resultat_id"), primary_key=True)
 )
