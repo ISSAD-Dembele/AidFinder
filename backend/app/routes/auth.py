300fe6d3cba6fe0e0cb04afc66fe8de232e949e9
ISSAD-Dembele <issaddembele25@gmail.com>
2026-06-30 04:44:05 +0100

    authentification fini

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -8,17 +8,17 @@ from app.services.auth_service import(register_user, login_user)
 
 router = APIRouter(
     prefix ="/auth",
-    tag =["Authentification"]
+    tags =["Authentification"]
 )
 
-@router.post("/register", reponse_model=UserReponse)
+@router.post("/register", response_model=UserReponse)
 def register(
     user:UserCreate,
     db:Session = Depends(get_db)
 ):
     return register_user(db, user)
 
-@router.post("/login", reponse_model=Token)
+@router.post("/login", response_model=Token)
 def login(
     user: UserLogin,
     db:Session = Depends(get_db)
